c7a643b66d236882fdb66fce763d343b48e32967
Otto <ottoemilio@gmail.com>
2020-07-22 10:13:10 +0200

    Initial commit

---
+++ main/it/enaip/cinema/Cinema.java
@@ -0,0 +1,10 @@
+package it.enaip.cinema;
+
+public class Cinema {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ main/it/enaip/cinema/SalaCinematografica.java
@@ -0,0 +1,5 @@
+package it.enaip.cinema;
+
+public class SalaCinematografica {
+
+}
