cf2fbe8c01064a4fbe316430ed17e0f3c7b48d47
Otto <ottoemilio@gmail.com>
2020-07-22 11:29:32 +0200

    Updated Biglietto

--- main/it/enaip/cinema/Biglietto.java
+++ main/it/enaip/cinema/Biglietto.java
@@ -1,5 +1,19 @@
 package it.enaip.cinema;
 
+/*La classe Biglietto contiene gli attributi posizione che indica il posto a sedere nella sala cinematografica ed il
+prezzo.
+ La classe biglietto contiene i metodi applicaRiduzioneAnziani() e calcolaRiduzioneBambini() per
+determinare la riduzione sul prezzo intero del biglietto.
+*/
 public class Biglietto {
-
+	int age;
+	float price;
+	float seatPrice;
+	public float discount (int age, float price) {
+		if (age > 60) {
+			seatPrice = (float) (price * 0.7);
+	}
+		return seatPrice;
+	 
+	}
 }
